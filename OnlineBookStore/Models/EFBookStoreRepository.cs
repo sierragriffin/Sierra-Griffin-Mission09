cb4e3d07e19d52a66894cc083f98496c2bc9c887
Sierra Griffin <sierra.griffin14@gmail.com>
2022-03-02 21:43:59 -0700

    Initial Commit

---
+++ OnlineBookStore/Models/EFBookStoreRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+
+namespace OnlineBookStore.Models
+{
+    public class EFBookStoreRepository : IBookStoreRepository
+    {
+        private BookstoreContext context { get; set; }
+
+        public EFBookStoreRepository(BookstoreContext temp)
+        {
+            context = temp;
+        }
+
+        public IQueryable<Book> Books => context.Books;
+    }
+}
+
+//implementation of interface that we implemented in "IBookStoreRepository.cs"
---
+++ OnlineBookStore/Models/IBookStoreRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+
+namespace OnlineBookStore.Models
+{
+    public interface IBookStoreRepository
+    {
+        //class set up specifically for querying data, very efficient
+        IQueryable<Book> Books { get; }
+    }
+}
+
+//an interface is not a class, it's a template for a class
